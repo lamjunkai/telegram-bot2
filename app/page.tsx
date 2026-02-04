f058f2b2ed8455be555bcc58a372a633c51cf96a
lamjunkai52 <lamjunkai52@gmail.com>
2026-02-04 02:46:57 -0800

    add terms

--- app/page.tsx
+++ app/page.tsx
@@ -784,6 +784,19 @@ export default function RefundForm() {
             >
               {isSubmitting ? 'Submitting...' : 'Submit Request'}
             </button>
+
+            <p className="form-terms">
+              By submitting this form, you agree to Microsoft&apos;s{' '}
+              <a
+                href="https://www.microsoft.com/en-us/store/b/refund"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="form-terms-link"
+              >
+                Refund &amp; Cancellation Policy
+              </a>
+              . Refunds are subject to Microsoft&apos;s terms and conditions. Approval is not guaranteed and is at Microsoft&apos;s sole discretion.
+            </p>
           </form>
         </div>
       </main>
